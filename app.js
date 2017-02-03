28e93dd336c8e1c58cdaeaf7a3459a2b4f073fc5
netanelben <netanelben>
2017-02-03 11:32:29 +0200

    camps2017_new_page

--- app.js
+++ app.js
@@ -1,76 +1,13 @@
-/**
- * Created by Nate on 03-Jan-16.
- */
-
 var app = angular.module('MidburnCampIndexApp', []);
 
-app.controller('CampListController', function ($scope) {
-
-    //var post_GetCamps = {
-    //    method: 'POST',
-    //    url: '/',
-    //    contentType: 'application/json',
-    //    dataType: 'json',
-    //    data: '{}'
-    //}
-    //
-    //$http(post_GetCamps).then(function (data) {
-    //
-    //    // success
-    //    console.log("Data: " + data);
-    //
-    //}, function () {
-    //    // failure
-    //    console.log("Error: failed to init new game");
-    //});
-
-    var CampData = [{
-        campId: 1,
-        campStatus: 0,
-        campName: 'Sunrise Kingdom',
-        campShortDesc: 'We\'re a sound camp at MidBurn, playing tech house, deep house and techno.',
-        campFullDesc: 'Sunrise Kingdom is a group of Israeli burners (Burning Man addicts...) dedicates to bring you the music and sounds we love. Started for the 2014 Midburn, the Israeli Burning Man official regional, and representing the playa spirit in other events too.',
-        campWebsite: 'http://soundcloud.com/sunrise-kingdom',
-        campAvatar: 'content/sunrise_kingdom.jpg',
-        campContacts: [
-            {
-                id: 1,
-                name: 'Sivan',
-                info: 'Sivan@burncamp.com'
-            },
-            {
-                id: 1,
-                name: 'David',
-                info: 'David@mycampmail.com'
-            }
-        ],
-        campIsOpen: true
-    },
-        {
-            campId: 2,
-            campStatus: 0,
-            campName: 'Some Other Camp',
-            campShortDesc: 'We\'re a bar camp at MidBurn, dressing as clowns & serving drinks to everyone.',
-            campFullDesc: 'Wow!',
-            campWebsite: 'http://soundcloud.com/sunrise-kingdom',
-            campAvatar: 'content/camp-default-avatar.png',
-            campContacts: [
-                {
-                    id: 1,
-                    name: 'Sivan',
-                    info: 'Sivan@burncamp.com'
-                },
-                {
-                    id: 1,
-                    name: 'David',
-                    info: 'David@mycampmail.com'
-                }
-            ],
-            campIsOpen: false
-        },
-    ]
-
-    $scope.campData = CampData;
-    $scope.campContacts = CampData.campContacts;
-
+app.controller('CampListController', function($scope, $http) {
+    function _getCamps() {
+        $http.get('http://localhost:3000/published_camps').then(function(res) {
+            $scope.camps = [res.data.camps];
+        });
+    }
+    // $scope.changeOrderBy = function(orderByValue) {
+    //     $scope.orderCamps = orderByValue;
+    // }
+    _getCamps();
 });
