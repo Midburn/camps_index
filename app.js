a71ee4f223b117232ed5d0caed49e8c558311105
netanelben <netanelben>
2017-02-03 14:35:08 +0200

    working version, english only

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ var app = angular.module('MidburnCampIndexApp', []);
 
 app.controller('CampListController', function($scope, $http) {
     function _getCamps() {
-        $http.get('http://localhost:3000/published_camps').then(function(res) {
-            $scope.camps = [res.data.camps];
+        $http.get('http://localhost:3000/camps_published').then(function(res) {
+            $scope.camps = res.data.published_camps;
+        }).catch(function(err) {
+            alert('No camps found.');
+        });
+    }
+
+    $scope._fetchContactPerson = function(id) {
+        $http.get('http://localhost:3000/camps_contact_person/' + id).then(function(res) {
+            user_info = res.data.user;
+            $scope.fullName = user_info.fullName;
+            $scope.email = user_info.email;
+            $scope.phone = user_info.phone;
+        }).catch(function(err) {
+            alert('No contacts found.');
         });
     }
     // $scope.changeOrderBy = function(orderByValue) {
