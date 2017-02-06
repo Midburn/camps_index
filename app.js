08a5ef791ec81ae1cacb3ce15a0f3b32a9af5782
netanelben <netanelben>
2017-02-06 18:54:07 +0200

    api host var

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var app = angular.module('MidburnCampIndexApp', []);
 
 app.controller('CampListController', function($scope, $http) {
+    var API_URL = 'http://54.194.247.12';
+
     function _getCamps() {
-        $http.get('http://localhost:3000/camps_published').then(function(res) {
+        $http.get(API_URL + '/camps_published').then(function(res) {
             $scope.camps = res.data.published_camps;
         }).catch(function(err) {
             alert('No camps found.');
@@ -10,7 +12,7 @@ app.controller('CampListController', function($scope, $http) {
     }
 
     $scope._fetchContactPerson = function(id) {
-        $http.get('http://localhost:3000/camps_contact_person/' + id).then(function(res) {
+        $http.get(API_URL + '/camps_contact_person/' + id).then(function(res) {
             user_info = res.data.user;
             $scope.fullName = user_info.fullName;
             $scope.email = user_info.email;
