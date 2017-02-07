7093e1f6758a7c17a2bdc726f4c587dc5b7248bf
--unset <nate@konimbo.co.il>
2017-02-07 13:20:00 +0200

    working solution

---
+++ public/app.js
@@ -0,0 +1,35 @@
+var app = angular.module('MidburnCampIndexApp', []);
+
+app.controller('CampListController', function($scope, $http) {
+    var API_URL = 'http://54.194.247.12';
+
+    function _getCamps() {
+        $http.get(API_URL + '/camps_published').then(function(res) {
+            $scope.camps = res.data.published_camps;
+        }).catch(function(err) {
+            alert('No camps found.');
+        });
+    }
+
+    /*$scope._fetchContactPerson = function(id) {
+        $http.get(API_URL + '/camps_contact_person/' + id).then(function(res) {
+            user_info = res.data.user;
+            $scope.fullName = user_info.fullName;
+            $scope.email = user_info.email;
+            $scope.phone = user_info.phone;
+        }).catch(function(err) {
+            alert('No contacts found.');
+        });
+    }*/
+    //_getCamps();
+
+    function _getCampsJSON() {
+        $http.get('/data.json').then(function(res) {
+            $scope.camps = res.data;
+        }).catch(function(err) {
+            alert('No camps found.');
+        });
+    }
+
+    _getCampsJSON();
+});
