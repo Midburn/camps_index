b923dbf50cdaca353bf513d80dda8668c35d1e84
--unset <nate@konimbo.co.il>
2017-02-12 18:25:54 +0200

    Animate on scroll, filtering results, scroll top link

--- public/app.js
+++ public/app.js
@@ -1,15 +1,88 @@
-var app = angular.module('MidburnCampIndexApp', ['ngSanitize']);
+/**
+ * Midburn camp index application
+ * @version: 1.1
+ * @author: nate ben
+ * @type {angularjs_app}
+ */
+var app = angular.module('camps_index', ['ngSanitize']);
 
 app.controller('CampListController', function($scope, $http) {
-    var API_URL = 'http://54.194.247.12';
+    var API_URL = 'http://54.194.247.12',
+        labels = {
+            en: {
+                search_placeholder: 'Serach a camp',
+                members: 'New members',
+                families: 'Accept families',
+                homepage: 'Homepage',
+                contact: {
+                    btn: 'Contact',
+                    name: 'Name',
+                    email: 'Email',
+                    phone: 'Phone'
+                },
+                misc: {
+                    theend: 'the end...',
+                    scrolltop: 'Back to top'
+                }
+            },
+            he: {
+                search_placeholder: 'חיפוש מחנה',
+                members: 'פתוח למצטרפים',
+                families: 'מקבל משפחות',
+                homepage: 'דף הבית',
+                contact: {
+                    btn: 'יצירת קשר',
+                    name: 'שם',
+                    email: 'אימייל',
+                    phone: 'טלפון'
+                },
+                misc: {
+                    theend: 'זה הסוף...',
+                    scrolltop: 'חזרה להתחלה'
+                }
+            }
+        },
+        lang = $('html').attr('lang');
 
+    // multi-language labels
+    $scope.lang = lang;
+    $scope.labels = labels[lang];
+
+    // init function
+    $scope.init = function() {
+        // init animations-on-scroll effect
+        AOS.init({
+            duration: 300
+        });
+    }
+
+    /**
+     * filters
+     */
+    $scope.sortBy = function(propertyName) {
+        // sorting filters for the list
+        $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+        $scope.propertyName = propertyName;
+        $('.camp__wrapper').slice('5').addClass('aos-animate');
+    };
+
+    // data fetch
     function _fetchCampsAPI() {
+        /**
+         * data fetch from Spark API
+         * @type {JSON}
+         */
         $http.get(API_URL + '/camps_published').then(function(res) {
             $scope.camps = res.data.published_camps;
         }).catch(function(err) {
             alert('No camps found.');
         });
     }
+
+    // serach callback
+    $scope.$watch('searchCamp', function() {
+        $('.camp__wrapper').slice('5').addClass('aos-animate');
+    });
 
     /*$scope._fetchContactPerson = function(id) {
         $http.get(API_URL + '/camps_contact_person/' + id).then(function(res) {
@@ -24,6 +97,10 @@ app.controller('CampListController', function($scope, $http) {
     //_fetchCampsAPI();
 
     function _getCampsJSON() {
+        /**
+         * data fetch JSON file
+         * @type {JSON}
+         */
         $http.get('/data.json').then(function(res) {
             $scope.camps = res.data.ToPublish;
         }).catch(function(err) {
@@ -34,6 +111,11 @@ app.controller('CampListController', function($scope, $http) {
     _getCampsJSON();
 });
 
-app.directive('camps-index', function() {
-    return {restrict: 'E', templateUrl: 'templates/list.html', replace: true, controller: 'CampListController'}
+app.directive('campsList', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/list.html',
+        replace: true,
+        controller: 'CampListController'
+    }
 });
