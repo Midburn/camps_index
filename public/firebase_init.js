479959e33b152eb5c275a4648c0ef441aa807655
Nate.Ben <mtr574@gmail.com>
2017-03-01 17:40:47 +0200

    loading spinner

---
+++ public/firebase_init.js
@@ -0,0 +1,7 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCw1qf_9X3Us0si8h71l7mrmgOb-B3x4cQ",
+    authDomain: "midburn-camps.firebaseapp.com",
+    databaseURL: "https://midburn-camps.firebaseio.com"
+};
+firebase.initializeApp(config);
