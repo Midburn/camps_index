9336692ffb9deb4b9472737bd4ba259a5c2ce70b
--unset <nate@konimbo.co.il>
2017-03-07 13:33:08 +0200

    WIP: change to firebaseObject

--- public/app.js
+++ public/app.js
@@ -1,12 +1,12 @@
 /**
  * Midburn camp index application
- * @version: 1.1
+ * @version: 1.2
  * @author: nate ben
  * @type {angularjs_app}
  */
 var app = angular.module('camps_index', ['ngSanitize', 'firebase']);
 
-app.controller('CampListController', function($scope, $http, $firebaseArray) {
+app.controller('CampListController', function($scope, $http, $firebaseObject) {
     var API_URL = 'http://54.194.247.12',
         labels = {
             en: {
@@ -51,7 +51,9 @@ app.controller('CampListController', function($scope, $http, $firebaseArray) {
     // init function
     $scope.init = function() {
         // init animations-on-scroll effect
-        AOS.init({duration: 300});
+        AOS.init({
+            duration: 300
+        });
     }
 
     /**
@@ -59,50 +61,63 @@ app.controller('CampListController', function($scope, $http, $firebaseArray) {
      */
     $scope.sortBy = function(propertyName) {
         // sorting filters for the list
-        $scope.reverse = ($scope.propertyName === propertyName)
-            ? !$scope.reverse
-            : true;
+        $scope.reverse = ($scope.propertyName === propertyName) ?
+            !$scope.reverse :
+            true;
         $scope.propertyName = propertyName;
         $('.camp__wrapper').slice('5').addClass('aos-animate');
     };
-
-    // data fetch
-    function _fetchCampsAPI() {
-        /**
-         * data fetch from Spark API
-         * @type {JSON}
-         */
-        $http.get(API_URL + '/camps_published').then(function(res) {
-            $scope.camps = res.data.published_camps;
-        }).catch(function(err) {
-            alert('No camps found.');
-        });
-    }
 
     // serach callback
     $scope.$watch('searchCamp', function() {
         $('.camp__wrapper').slice('5').addClass('aos-animate');
     });
 
-    function _getCampsJSON() {
-        /**
-         * fetch data from Firebase:Database
-         * @type {JSON}
-         */
-        var ref = firebase.database().ref(),
-            camps_array = $firebaseArray(ref);
-        $scope.camps = camps_array;
+    function fetchCampsData() {
 
-        // loading callback
-        camps_array.$loaded().then(function(data) {
-            // loading animation
-            $('#loading_spinner').remove();
-        }).catch(function(error) {
-            _fetchFromJSON();
-        });
+        function _fetchFromSparkAPI() {
+            /**
+             * data fetch from Spark API
+             * @type {JSON}
+             */
+            $http.get(API_URL + '/camps_published').then(function(res) {
+                $scope.camps = res.data.published_camps;
+            }).catch(function(err) {
+                alert('No camps found.');
+            });
+        }
 
-        // data JSON fallback
+        (function _fetchFromFirebase() {
+            /**
+             * fetch data from Firebase:Database
+             * @type {JSON}
+             */
+            var ref = firebase.database().ref('ToPublish'),
+                data_object = $firebaseObject(ref);
+
+            // loading callback
+            data_object.$loaded().then(function(data) {
+                // bind data to DOM
+                var camps = []
+                angular.forEach(data, function(key, value) {
+                    camps.push({
+                        key: value
+                    })
+                })
+                $scope.camps = camps;
+
+                // loading animation
+                $('#loading_spinner').remove();
+            }).catch(function(error) {
+                _fetchFromJSON();
+            });
+        })()
+
         function _fetchFromJSON() {
+            /**
+             * fallback method, fetch data from local JSON file
+             * @type {JSON}
+             */
             $http.get('/data.json').then(function(res) {
                 $scope.camps = res.data.ToPublish;
             }).catch(function(err) {
@@ -111,9 +126,14 @@ app.controller('CampListController', function($scope, $http, $firebaseArray) {
         }
     }
 
-    _getCampsJSON();
+    fetchCampsData();
 });
 
 app.directive('campsList', function() {
-    return {restrict: 'E', templateUrl: 'templates/list.html', replace: true, controller: 'CampListController'}
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/list.html',
+        replace: true,
+        controller: 'CampListController'
+    }
 });
