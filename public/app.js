2e830087df6031d6d27432fb0f108350c38769bb
natekonimbo <nate@konimbo.co.il>
2017-03-30 19:38:26 +0300

    WIP: fetch data from SparkAPI -- adapt camp.keys in view -- NOT DEPLOYED

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@
 var app = angular.module('camps_index', ['ngSanitize', 'firebase']);
 
 app.controller('CampListController', function($scope, $http, $firebaseObject) {
-    var API_URL = 'http://54.194.247.12',
+    var API_URL = 'http://sparkstaging.midburn.org/api/v1/camps/published',
         labels = {
             en: {
                 search_placeholder: 'Serach a camp',
@@ -73,44 +73,22 @@ app.controller('CampListController', function($scope, $http, $firebaseObject) {
         $('.camp__wrapper').slice('5').addClass('aos-animate');
     });
 
-    function fetchCampsData() {
-        // function _fetchFromSparkAPI() {
-        //     /**
-        //      * data fetch from Spark API
-        //      * @type {JSON}
-        //      */
-        //     $http.get(API_URL + '/camps_published').then(function(res) {
-        //         $scope.camps = res.data.published_camps;
-        //     }).catch(function(err) {
-        //         alert('No camps found.');
-        //     });
-        // }
-
-        // (function _fetchFromFirebase() {
-        //     /**
-        //      * fetch data from Firebase:Database
-        //      * @type {JSON}
-        //      */
-        //     var ref = firebase.database().ref('ToPublish'),
-        //         data_object = $firebaseObject(ref);
-        // 
-        //     // loading callback
-        //     data_object.$loaded().then(function(data) {
-        //         // bind data to DOM
-        //         var camps = []
-        //         angular.forEach(data, function(key, value) {
-        //             camps.push({
-        //                 key: value
-        //             })
-        //         })
-        //         $scope.camps = camps;
-        // 
-        //         // loading animation
-        //         $('#loading_spinner').remove();
-        //     }).catch(function(error) {
-        //         _fetchFromJSON();
-        //     });
-        // })()
+    (function fetchCampsData() {
+        function _fetchFromSparkAPI() {
+            /**
+             * data fetch from Spark API
+             * @type {JSON}
+             */
+            $http.get(API_URL).then(function(res) {
+                if (res.data.quantity === 0) {
+                  alert('Zero camps found.');
+                } else {
+                  $scope.camps = res.data.camps;
+                }
+            }).catch(function(err) {
+                alert('Error fetching camps.');
+            });
+        }
 
         function _fetchFromJSON() {
             /**
@@ -124,11 +102,8 @@ app.controller('CampListController', function($scope, $http, $firebaseObject) {
                 alert('No camps found.');
             });
         }
-        
-        _fetchFromJSON();
-    }
-
-    fetchCampsData();
+        _fetchFromSparkAPI();
+    })()
 });
 
 app.directive('campsList', function() {
